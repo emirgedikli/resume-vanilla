69588eddd1e6b161821cecc976287b4d9e9a7954
emirgedikli <emirwtaha@gmail.com>
2022-09-28 12:18:28 +0200

    fonts

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import "./assets/css/styles.css";
 import "@fortawesome/fontawesome-free/js/all";
+
+$(function(){
+    $(window).scroll(function(){
+      var winTop = $(window).scrollTop();
+      if(winTop >= 30){
+        $("body").addClass("sticky-header");
+      }else{
+        $("body").removeClass("sticky-header");
+      }//if-else
+    });//win func.
+  });//ready func.
