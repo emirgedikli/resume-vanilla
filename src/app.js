0724efe19e6b7523b7133f5f1050b2fde7ba174c
emirgedikli <emirwtaha@gmail.com>
2022-09-29 16:24:56 +0200

    css html

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import "./assets/css/styles.css";
-import "@fortawesome/fontawesome-free/js/all";
+import "./assets/font-awesome-4.7.0/css/font-awesome.min.css"
 
 $(function(){
     $(window).scroll(function(){
