72182361be72b68874511a57ca9090f98236cd25
emirgedikli <emirwtaha@gmail.com>
2022-10-02 00:14:16 +0200

    responsive design finished

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import "./assets/css/styles.css";
 import "./assets/font-awesome-4.7.0/css/font-awesome.min.css"
 
-$(function(){
-    $(window).scroll(function(){
-      var winTop = $(window).scrollTop();
-      if(winTop >= 30){
-        $("body").addClass("sticky-header");
-      }else{
-        $("body").removeClass("sticky-header");
-      }//if-else
-    });//win func.
-  });//ready func.
+// $(function(){
+//     $(window).scroll(function(){
+//       var winTop = $(window).scrollTop();
+//       if(winTop >= 30){
+//         $("body").addClass("sticky-header");
+//       }else{
+//         $("body").removeClass("sticky-header");
+//       }//if-else
+//     });//win func.
+//   });//ready func.
